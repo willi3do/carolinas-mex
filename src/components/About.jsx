1a4c2ecaa36247e6756e243b87164d0a452ae854
willi3do <dominguez.william@yahoo.com>
2026-04-04 08:48:07 -0700

    refactor: simplify About component by extracting story into a variable

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,29 @@
 export default function About() {
+  const story = (
+    <p>
+      My parents, tia's, tios & abuelos, immigrated to the US from Michoácan,
+      Mexico. We all grew up in a small home and each family acquired one room.
+      That may sound a bit chaotic, & even though it was at times, I love the
+      way I grew up. No matter how much or how little we had, we always had a
+      homecooked meal. Whether dinner was rice and beans or a much more
+      time-consuming meal, it was always made with love. Being Mexican American,
+      I learned to cook from all the amazing women in my life including my mom,
+      my tia, and my sister. As an adult, I have had the opportunity to create
+      meals that make people happy. My husband, Bobby aka Papa, saw my passion
+      in the kitchen & convinced me to quit my job in accounting to pursue my
+      dreams. Thanks to him, Carolina's was created. Today, you're going to try
+      food that's authentically me. Whether you're joining us for lunch or
+      dinner, rest assured that your meal will always be hecho con amor. We are
+      happy to be one of the few restaurants in Las Vegas that are Seed Oil
+      Free! We proudly use fresh ingredients, the highest quality meats, and
+      just like the food I ate growing up, everything is made from scratch.
+      Siempre hecho con amor, Carolina"
+    </p>
+  );
   return (
     <div className="about-section">
       <h2>About Us</h2>
-      <p>
-        My parents, tia's, tios & abuelos, immigrated to the US from Michoácan,
-        Mexico. We all grew up in a small home and each family acquired one
-        room. That may sound a bit chaotic, & even though it was at times, I
-        love the way I grew up. No matter how much or how little we had, we
-        always had a homecooked meal. Whether dinner was rice and beans or a
-        much more time-consuming meal, it was always made with love. Being
-        Mexican American, I learned to cook from all the amazing women in my
-        life including my mom, my tia, and my sister. As an adult, I have had
-        the opportunity to create meals that make people happy. My husband,
-        Bobby aka Papa, saw my passion in the kitchen & convinced me to quit my
-        job in accounting to pursue my dreams. Thanks to him, Carolina's was
-        created. Today, you're going to try food that's authentically me.
-        Whether you're joining us for lunch or dinner, rest assured that your
-        meal will always be hecho con amor. We are happy to be one of the few
-        restaurants in Las Vegas that are Seed Oil Free! We proudly use fresh
-        ingredients, the highest quality meats, and just like the food I ate
-        growing up, everything is made from scratch. Siempre hecho con amor,
-        Carolina"
-      </p>
+      <p>{story}</p>
     </div>
   );
 }
