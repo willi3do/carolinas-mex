9fdfcf2a3bae9e52e2126f38c087c1053558d1e4
willi3do <dominguez.william@yahoo.com>
2026-05-21 23:44:30 -0700

    feat: enhance Nav component styling and update Home page content

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,41 +7,44 @@ export default function Nav() {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="sticky top-0 flex min-w-full shadow-md z-100 items-center m-1">
-      <div className="flex justify-between w-[99%] items-center p-2 gap-4  bg-amber-500">
+    <nav className="sticky top-0 flex h-auto min-w-full shadow-md z-100  ">
+      <div className="flex justify-around w-full  items-center bg-amber-500 border-2">
         <img
-          className="border-red-500 border-2 object-cover w-15 h-sm object-center"
-          src="/images/kiss-5.jpg "
+          className=" p-1 rounded-3xl object-cover w-20 h-32.5 object-center"
+          src="/Images/hero.jpg"
           alt="Carolina's Mex Kitchen hero image"
         />
         <Link
           to="/"
-          className="text-2xl font-bold text-amber-900 font-cookie p-2 rounded"
+          className="text-6xl text-outline leading-tight font-cookie"
         >
           Carolinas Mexican Kitchen
         </Link>
         <button
-          className="flex flex-col gap-1.5 cursor-pointer border-none bg-amber-900 p-3 rounded "
+          className="flex flex-col gap-1.5 cursor-pointer border bg-white p-2 rounded "
           onClick={toggleMenu}
           aria-label="Toggle menu"
         >
-          <span className="w-6 h-1 bg-green-900 block rounded"></span>
-          <span className="w-6 h-0.5 bg-white block rounded"></span>
-          <span className="w-6 h-0.5 bg-red-700 block rounded"></span>
+          <span className="w-6 h-1 bg-black block rounded"></span>
+          <span className="w-6 h-1 bg-black block rounded"></span>
+          <span className="w-6 h-1 bg-black block rounded"></span>
         </button>
       </div>
       {isOpen && (
-        <div className="bg-white border-t border-gray-200">
+        <div
+          className=" bg-white flex flex-col border-gray-200 
+         text-sm"
+        >
           <Link
             to="/"
-            className="block px-6 py-3 text-gray-800 font-medium hover:bg-amber-50 hover:text-amber-600 transition-colors"
+            className="block px-6 py-3 text-gray-800 text-sm hover:bg-amber-50 hover:text-amber-600 transition-colors"
             onClick={() => setIsOpen(false)}
           >
             Home
           </Link>
           <Link
             to="/menu"
-            className="block px-6 py-3 text-gray-800 font-medium hover:bg-amber-50 hover:text-amber-600 transition-colors"
+            className="block px-6 py-3 text-gray-900 hover:bg-amber-50 hover:text-amber-600 transition-colors"
             onClick={() => setIsOpen(false)}
           >
             Menu
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,22 @@
 
 export default function Home() {
   return (
-    <div className=" flex min-h-full min-w-full justify-center overflow-hidden rounded-b-sm bg-gray-500">
+    <div className=" flex min-h-full min-w-full justify-center overflow-x-hidden  bg-gray-500 mt-5">
       <div className="container border-orange-800 w-full">
-        <div className="text-center mb-4 bg-stone-400">
-          <h1 className="font-bold text-gray-800 mb-4"> Restaurant name</h1>
+        <section id="hero" className="text-center mb-4 bg-stone-400">
+          <div id="hero-left">
+            <div id="hero-eyebrow" className="text-md">
+              {" "}
+              Made daily with amor
+            </div>
+          </div>
 
           <p className=" text-sm text-gray-800">
             Experience the flavors of traditional Mexican cooking with fresh
-            ingredients and authentic recipes.
+            ingredients and authentic recipes. We are seed oil free kitchen.
           </p>
-        </div>
+        </section>
+
         <div className="bg-white rounded-lg shadow-md p-6 mx-2 h-[50%] flex justify-center items-center">
           <h2 className="text-2xl font-semibold text-gray-700 mb-20 "></h2>
           <p className="text-3xl text-gray-600">
