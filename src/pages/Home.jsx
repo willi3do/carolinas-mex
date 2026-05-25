5164e9ea4a545b278159d70d5245d26f6a0a27c3
willi3do <dominguez.william@yahoo.com>
2026-05-24 22:50:24 -0700

    feat: replace birria eggrolls image and update Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-// import birria from "/birriaEggRolls.jpg";
+// import birria from "../public/Images/birriaEggRolls.jpg";
 // import hero from "/images/hero.jpg";
 
 export default function Home() {
@@ -6,30 +6,29 @@ export default function Home() {
     <div className=" flex min-h-full min-w-full justify-center overflow-x-hidden  bg-gray-500 mt-5">
       <div className="container border-orange-800 w-full">
         <section id="hero" className="text-center mb-4 bg-stone-400">
-          <div id="hero-left">
-            <div id="hero-eyebrow" className="text-md">
+          <div id="hero-left"></div>
+        </section>
+
+        <section
+          id="heroSection"
+          style={{ backgroundImage: `url(/Images/birria-eggrolls.jpeg)` }}
+          className="bg-cover bg-center bg-no-repeat min-h-screen relative p-10 "
+        >
+          <div className="absolute inset-0 bg-black/50"></div>
+          <div id="heroContainer" className="bg-black">
+            <div id="hero-eyebrow" className="text-lg text-center text-white">
               {" "}
               Made daily with amor
             </div>
+
+            <p className=" text-sm text-white">
+              Experience the flavors of traditional Mexican cooking with fresh
+              ingredients and authentic recipes. We are seed oil free kitchen.
+            </p>
           </div>
 
-          <p className=" text-sm text-gray-800">
-            Experience the flavors of traditional Mexican cooking with fresh
-            ingredients and authentic recipes. We are seed oil free kitchen.
-          </p>
+          <div className=" rounded-lg shadow-md p-6 mx-2 h-[50%] flex justify-center items-center"></div>
         </section>
-
-        <div className="bg-white rounded-lg shadow-md p-6 mx-2 h-[50%] flex justify-center items-center">
-          <h2 className="text-2xl font-semibold text-gray-700 mb-20 "></h2>
-          <p className="text-3xl text-gray-600">
-            Authentic Mexican cuisine made with love
-          </p>
-          {/* <img
-              className="bg-red w-2xl object-cover-full"
-              src={birria}
-              alt="Birria egg rolls"
-            /> */}
-        </div>
       </div>
     </div>
   );
