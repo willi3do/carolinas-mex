56d9d0983959e7462c19330d2fb9371c71824ddb
willi3do <dominguez.william@yahoo.com>
2026-05-29 09:06:15 -0700

    feat: add favorites section and about restaurant content to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const favoritesSection = [
+    { id: 1, name: "item1", image: "Image route" },
+    { id: 2, name: "item2", image: "Image route" },
+    { id: 3, name: "item3", image: "Image route" },
+    { id: 4, name: "item4", image: "Image route" },
+    { id: 5, name: "item5", image: "Image route" },
+    { id: 6, name: "item6", image: "Image route" },
+  ];
   return (
-    <div className=" flex min-h-full min-w-full justify-center overflow-x-hidden ">
+    <div className=" flex flex-col min-h-full min-w-full justify-center overflow-x-hidden ">
       <section
         id="heroSection"
         style={{
@@ -30,8 +38,49 @@ export default function Home() {
               Order now
             </button>
           </Link>
+
+          <Link to="/reservations">
+            <button className="bg-green-500 hover:bg-stone-300 text-white font-bold py-4 px-7 rounded m-5">
+              Reserve a table
+            </button>
+          </Link>
         </div>
       </section>
+
+      <section id="favoritesSection" className="bg-amber-500 p-10">
+        <h1 className="text-4xl text-center font-bold mb-10">
+          Popular Menu Items
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+          {favoritesSection.map((item) => (
+            <div key={item.id} className="flex flex-col items-center gap-1">
+              <img
+                src={"item.image"}
+                alt={item.name}
+                className="w-1/2 h-20 bg-cyan-400 object-cover rounded-md  hover:bg-stone-300"
+              />
+              <p className="text-lg font-semibold">{item.name}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section id="aboutRestaurant">
+        From our familia to yours. My family immigrated from Michoacán, Mexico,
+        and no matter how much or how little we had, there was always a
+        homecooked meal made with love. Carolina’s was born from that same
+        spirit — authentic, from-scratch cooking passed down from the women who
+        raised me. Every dish is made with the freshest ingredients, the highest
+        quality meats, and zero seed oils. Siempre hecho con amor. 🤍
+      </section>
+
+      <section id="reviewsAndTestimonials">
+        {/* Reviews and testimonials content would go here */}
+      </section>
+
+      <section id="locationAndHours"></section>
+
+      <section id="footer"></section>
     </div>
   );
 }
